6d65042dc363e7bffcc40c76fff995b2c049e953
Kamil Kuczaj <218478@student.pwr.edu.pl>
2017-11-03 19:29:09 +0100

    Loading regardless of img skew. Dataset generation
    It's now possible to generate the dataset youself. The function
    to rotate the text according to skew has also been added.
    GUI has to be multithreaded

--- src/runGUI.py
+++ src/runGUI.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QStyleFactor
 from PyQt5.QtGui import QPixmap, QImage, QPalette, QColor
 from PyQt5.QtCore import Qt
 import argparse, sys, os, cv2
+import numpy as np
 
 from design import Ui_MainWindow
 from cnn import modelCNN
@@ -79,8 +80,52 @@ class myGUI(QMainWindow):
         height, width = img.shape[:2]
         return QImage(img, width, height, QImage.Format_RGB888)
 
+    def takeCareOfImageRotation(self, image):
+        """
+        https://www.pyimagesearch.com/2017/02/20/text-skew-correction-opencv-python/
+        """
+        if len(image.shape) > 2:
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            gray = cv2.bitwise_not(gray)
+        else:
+            gray = cv2.bitwise_not(image)
+
+        # threshold the image, setting all foreground pixels to
+        # 255 and all background pixels to 0
+        thresh = cv2.threshold(gray, 0, 255,
+            cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+        # grab the (x, y) coordinates of all pixel values that
+        # are greater than zero, then use these coordinates to
+        # compute a rotated bounding box that contains all
+        # coordinates
+        coords = np.column_stack(np.where(thresh > 0))
+        angle = cv2.minAreaRect(coords)[-1]
+
+        # the `cv2.minAreaRect` function returns values in the
+        # range [-90, 0); as the rectangle rotates clockwise the
+        # returned angle trends to 0 -- in this special case we
+        # need to add 90 degrees to the angle
+        if angle < -45:
+            angle = -(90 + angle)
+
+        # otherwise, just take the inverse of the angle to make
+        # it positive
+        else:
+            angle = -angle
+
+        # rotate the image to deskew it
+        (h, w) = image.shape[:2]
+        center = (w // 2, h // 2)
+        M = cv2.getRotationMatrix2D(center, angle, 1.0)
+        rotated = cv2.warpAffine(image, M, (w, h),
+            flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+        return rotated
+
+
     def doOCR(self):
         self.extractTextFromSelectedFile()
+        self.tE.image = self.takeCareOfImageRotation(self.tE.image)
         self.ui.labelImageAfterOCR.setPixmap(QPixmap.fromImage(self.cvtCvMatToQImg(self.tE.image).scaled(self.ui.labelImageAfterOCR.width(), self.ui.labelImageAfterOCR.height())))
         if self.ui.comboBoxAlgorithms.currentIndex() == 0:
             self.reader = Reader_Chars74K()
--- utils/create_my_dataset.py
+++ utils/create_my_dataset.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
+import imgaug as ia
 from imgaug import augmenters as iaa
-import os, cv2, sys
+import os, cv2, sys, time
 import numpy as np
 
 
@@ -20,7 +21,7 @@ class DatasetCreator():
     def __init__(self, height = 16, width = 16, classNo=62):
         self.height = height
         self.width = width
-        self.classNo = 62
+        self.classNo = classNo
         self.createReadableLabels()
 
     def _create_blank(self, width, height, rgb_color=(0, 0, 0)):
@@ -42,42 +43,88 @@ class DatasetCreator():
         for i in range(97,123):
             self.readableLabels[i-61] =  chr(i)
 
-    def generateDataset(self, rootPath, fontscale = 1, count = 1):
-        seq = iaa.Sequential([
+    def commonAugmentation(self):
+        '''
+        iaa.Sequential([
                 iaa.Crop(px=(0, 1)), # crop images from each side by 0 to 16px (randomly chosen)
-                iaa.GaussianBlur(sigma=(0, 3.0)) # blur images with a sigma of 0 to 3.0
-                ])
+                iaa.GaussianBlur(sigma=(0, 1.0)), # blur images with a sigma of 0 to 3.0
+                iaa.Sharpen(alpha=1.0)
+                ], random_order=True)
+
+        http://imgaug.readthedocs.io/en/latest/source/examples_basics.html
+        '''
+        ia.seed(1)
+        seq = iaa.Sequential([
+            iaa.Crop(percent=(0, 0.1)), # random crops
+            # Small gaussian blur with random sigma between 0 and 0.5.
+            # But we only blur about 50% of all images.
+            iaa.Sometimes(0.5,
+                iaa.GaussianBlur(sigma=(0, 0.3))
+            ),
+            # Strengthen or weaken the contrast in each image.
+            iaa.ContrastNormalization((0.875, 1.125)),
+            # Add gaussian noise.
+            # For 50% of all images, we sample the noise once per pixel.
+            # For the other 50% of all images, we sample the noise per pixel AND
+            # channel. This can change the color (not only brightness) of the
+            # pixels.
+            iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+            # Make some images brighter and some darker.
+            # In 20% of all cases, we sample the multiplier once per channel,
+            # which can end up changing the color of the images.
+            iaa.Multiply((0.9, 1.1), per_channel=0.1),
+            # Apply affine transformations to each image.
+            # Scale/zoom them, translate/move them, rotate them and shear them.
+            iaa.Affine(
+                scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+                rotate=(-2.5, 2.5),
+                shear=(-8, 8)
+            )
+        ], random_order=True) # apply augmenters in random order
+        return seq
+
+
+    def generateDataset(self, rootPath, fontscale = 0.5, countForClass = 1016):
+        seq = self.commonAugmentation()
+        sys.stdout.write("[%s]" % (" " * self.classNo))
+        sys.stdout.flush()
+        sys.stdout.write("\b" * (self.classNo+1)) # return to start of line, after '['
         for c in range(self.classNo):
             suffix = "%03d" % (c+1)
             dir_name = os.path.join(rootPath, "Sample" + suffix)
             if not os.path.exists(dir_name):
                 os.makedirs(dir_name)
-            for i in range(count):
+            for i in range(countForClass):
                 # 'images' should be either a 4D numpy array of shape (N, height, width, channels)
                 # or a list of 3D numpy arrays, each having shape (height, width, channels).
                 # Grayscale images must have shape (height, width, 1) each.
                 # All images must have numpy's dtype uint8. Values are expected to be in
                 # range 0-255.
-                images = self._putCvText(self.readableLabels[c], fontscale=fontscale, howManyInstances=count)
-                # images_aug = seq.augment_images(images)
-                for idx, img_aug in enumerate(images):
+                images = self._putCvText(self.readableLabels[c], fontscale=fontscale, howManyInstances=countForClass)
+                images_aug = seq.augment_images(images)
+                for idx, img_aug in enumerate(images_aug):
                     filename = "image" + suffix + "%05d.png"% idx
-                    # cv2.imwrite(os.path.join(dir_name, filename),img_aug)
+                    cv2.imwrite(os.path.join(dir_name, filename),img_aug)
+            sys.stdout.write("-")
+            sys.stdout.flush()
+
+        sys.stdout.write("\n")
 
     def _putCvText(self, txt, fontscale, howManyInstances=1016, bckgColor = (255, 255, 255), fontColor = (0, 0, 0)):
         image = self._create_blank(self.width, self.height, rgb_color=bckgColor)
-        font = cv2.FONT_HERSHEY_COMPLEX #  normal sans serif
-        cv2.putText(image, str(txt), (int(self.height/2), int(self.width/2)), font, fontscale, fontColor)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(image, str(txt), (int(self.height/4), int(self.width/5*4)), font, fontscale, fontColor)
         images = np.zeros((howManyInstances,self.height, self.width, 3))
         for i in range(howManyInstances): # TODO: I bet this can be nicely optimized
             images[i] = image
-
-        print(image)
         return images
 
 def main():
-    d = DatasetCreator(16,16)
-    d.generateDataset("dataset")
-
+    start = time.clock()
+    d = DatasetCreator(height=16, width=16, classNo=62)
+    d.generateDataset("dataset",fontscale = 0.5, countForClass=10)
+    stop = time.clock()
+    print("It took me %f seconds" % (stop-start))
 if __name__ == '__main__':
     main()
